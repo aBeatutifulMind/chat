6ab7db4a644bfa881611d2775a8f03895b0344a2
junyyy <67784390+junyyy@users.noreply.github.com>
2020-09-10 13:43:32 +1000

    Delete chatmessage.sql

--- sql/chatmessage.sql
+++
@@ -1,7 +0,0 @@
-create table chatmessage(
-id int(10) unsigned not null auto_increment primary key,
-message varchar(4000) not null,
-username varchar(100) not null,
-msgtime datetime not null,
-colorselected varchar(45) not null
-);
